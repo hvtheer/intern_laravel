b6313826a4325f2a5c31cad4e9b994dca380068d
hvtheer <hthe1010@gmail.com>
2022-10-19 09:58:22 +0700

    Added views, styles

---
+++ resources/views/layouts/layout.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    @include('layouts.head')
+</head>
+<body>
+    @include('layouts.header')
+    @include('layouts.sidebar')
+    @yield('content')
+    @include('layouts.footer')
+</body>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.bundle.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+</html>
